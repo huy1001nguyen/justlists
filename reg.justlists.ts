75be604c2b72f7a44de4cf635059a19724cee256
RegFinance <82139353+RegFinance@users.noreply.github.com>
2021-04-13 11:03:19 +0700

    Create reg.justlists.ts

---
+++ reg.justlists.ts
@@ -0,0 +1,20 @@
+{
+"name":"JustSwap REG TRC10 ",
+"tokens":[
+{
+"symbol":"REG",
+"address":"TPycf1hKwk4ndCWNQ5LuAAR2Ko8hNGLBRR",
+"chainId":1,
+"decimals":6,
+"name":"Reg Finance",
+"logoURI":"https://coin.top/production/upload/logo/1003886.png?t=1617948485847"
+},
+
+],
+"logoURI":"https://justswap.io/favicon.ico",
+"version":{
+    "patch":1,
+    "major":1,
+    "minor":0
+},
+}
